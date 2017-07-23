23c1c4d38297c264138a1b54d84913eb6175f584
RacharlaNeha <nracharla22@gmail.com>
2017-07-23 22:23:38 +0000

    added search nodes criteria

--- cookbooks/postgresql/recipes/default.rb
+++ cookbooks/postgresql/recipes/default.rb
@@ -4,8 +4,8 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-package 'postgresql-server' do
-        notifies :run,'execute[postgresql-init]'
+package 'postgresql-server' do 
+        notifies :run,'execute[postgresql-init]', :immediately
 end
 
 
